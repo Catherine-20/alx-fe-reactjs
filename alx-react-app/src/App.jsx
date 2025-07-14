0867b51ae917a0b76ba061ee4cd15272baea9773
Catherine <catherineingabire15@gmail.com>
2025-07-14 14:02:01 +0300

    Add WelcomeMessage component and integrate it into App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,11 @@ import WelcomeMessage from './components/WelcomeMessage';
 
 function App() {
   return (
-    <>
+    <div>
       <WelcomeMessage />
-    </>
+    </div>
   );
 }
 
 export default App;
+
