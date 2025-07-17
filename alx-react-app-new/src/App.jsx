4a06bbd671957e3a251d23e373cb51f74a1d071d
Catherine <catherineingabire15@gmail.com>
2025-07-17 16:18:59 +0300

    Fix: add alx-react-app-new as normal folder

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <UserProfile name="Kath" age="21" bio="Loves coding and exploring new cities." />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
